32e61bfb075c9bbb33650a7a629598d1386e06c3
Diego Lain <diegolain.personal@gmail.com>
2024-02-09 18:33:53 +0100

    Base
    Actividad Odoo - Inicio

---
+++ on_click/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class OnClick(http.Controller):
+#     @http.route('/on_click/on_click', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/on_click/on_click/objects', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('on_click.listing', {
+#             'root': '/on_click/on_click',
+#             'objects': http.request.env['on_click.on_click'].search([]),
+#         })
+
+#     @http.route('/on_click/on_click/objects/<model("on_click.on_click"):obj>', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('on_click.object', {
+#             'object': obj
+#         })
